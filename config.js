6372684440c4a93d4f327af19bbbd0d625116202
s-munoz <85895242+s-munoz@users.noreply.github.com>
2025-04-29 12:36:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,7 @@ const redirectUrl =
                     //'https://grupogesonline.com/mur-15-y56';
                     
                     //A. Park
-                    'https://grupogesonline.com/alk-14-o02';
+                    //'https://grupogesonline.com/alk-14-o02';
+
+//prueba
+'youtube.com';
