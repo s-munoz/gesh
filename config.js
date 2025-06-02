aa70fc8210fc1c3eeafcde4d797ec35ab44d4c87
s-munoz <85895242+s-munoz@users.noreply.github.com>
2025-06-02 22:49:03 +0200

    Actualizar config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const redirectUrl =
                     //'https://grupogesonline.com/mur-15-y56';
 
                     //A. Playa
-                    'https://grupogesonline.com/abh-18-e99';
+                    //'https://grupogesonline.com/abh-18-e99';
   
                     //A. Park
-                    // 'https://grupogesonline.com/alk-14-a44';
+                     'https://grupogesonline.com/alk-14-a44';
 
                     //Ciudad Blanca
                     //'https://grupogesonline.com/bln-73-74u';
