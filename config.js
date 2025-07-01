c4dcc62e12635a5f5e9cd241387317689aa69e58
s-munoz <85895242+s-munoz@users.noreply.github.com>
2025-07-01 19:46:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,14 @@ const redirectUrl =
                     //'https://grupogesonline.com/abh-18-e99';
   
                     //A. Park
-                    //'https://grupogesonline.com/alk-14-a44';
+  
+                    'https://grupogesonline.com/alk-14-x23';
 //'https://grupogesonline.com/aba-17-51p';
 
                     //Ciudad Blanca
                     //'https://grupogesonline.com/bln-73-74u';
 
-'https://grupogesonline.com/pmv-107-s33';
+//'https://grupogesonline.com/pmv-107-s33';
 
 //prueba
 //'http://youtube.com';
