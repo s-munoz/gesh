afd3a081528ad4c2a26cc775012f8acd1b65810e
s-munoz <85895242+s-munoz@users.noreply.github.com>
2026-03-03 20:12:16 +0100

    Refactor redirect URLs and comments in config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const redirectUrl = 
                     //P. Muro
-                    //'https://grupogesonline.com/mur-15-y56';
+                    'https://grupogesonline.com/mur-15-y56';
 
 //'https://grupogesonline.com/pmv-107-s33';
                     //A. Playa
-                    'https://grupogesonline.com/abh-18-e99';
+                    //'https://grupogesonline.com/abh-18-e99';
 
                     //A. Park
   
                     //'https://grupogesonline.com/alk-14-x23';
 //'https://grupogesonline.com/aba-17-51p';
 
-                    //Ciudad Blanca
+                    //C. Blanca
                     //'https://grupogesonline.com/bln-73-74u';
 
 //'https://grupogesonline.com/pmv-107-s33';
